4a1bbf3c54a87ac00267d23bcc5226bc52593d11
Eugene <yauheni_zinchanka@epam.com>
2018-03-02 04:10:32 +0300

    Implemented function for calculated how many zeros needed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+// Stirling formula
+// function stirlingFormula(n) {
+//   return Math.sqrt(2*Math.PI*n)* Math.pow(n/Math.E, n)
+// }
+
+// 1! = 1
+// 2! = 2
+// 3! = 6
+// 4! = 24
+// 5! = 120 -> 1 
+
+var result = 0
+
+if (number > 1 && number < Math.pow(10,8)) {
+  const step = 5
+  var index = step
+
+  do {
+    value = number/index    
+    result += value >> 0.5
+    index = index * step      
+  } 
+  while (value >= 1) 
+
+}
+
+return result
 }
